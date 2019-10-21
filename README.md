1d2d6996026c871ec4b178797faa9e7b51a2c6c6
asketak <simatomas@seznam.cz>
2019-10-22 01:12:55 +0200

    contract stub

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # chainlink-predictall
+
+Predictall is a platform, where anyone can build their prediction market using smart contracts and chainlink to let people bet on the outcome of whatever api call he wants to.
+
+
+## Usage
--- contracts/PredictAll.sol
+++ contracts/PredictAll.sol
@@ -1,6 +1,6 @@
-pragma solidity >=0.4.25 <0.6.0;
+pragma solidity 0.4.24;
 
-import "./ConvertLib.sol";
+import "chainlink/contracts/ChainlinkClient.sol";
 
 // This is just a simple example of a coin-like contract.
 // It is not standards compatible and cannot be expected to talk to other
@@ -31,4 +31,29 @@ contract MetaCoin {
 	function getBalance(address addr) public view returns(uint) {
 		return balances[addr];
 	}
+}
+
+/**
+ * The PredictMarket contract is one market, where people can bet on outcome of some api
+ */
+contract PredictMarket {
+	string public name = "How many tweets wil @potus post between 30.10 and 7.11";
+	string public api_path = "How many tweets wil @potus post between 30.10 and 7.11";
+	string public http_post_or_get = "GET";
+	string public post_data = "{  auth: PREDICTOR_AUTH_TOKEN}";
+	string public auth_token = "";
+	uint256 public market_finalize_after = 0;
+
+  	constructor(string _auth_token_hash) public {
+  		auth_token_hash = _auth_token_hash
+
+ 	}
+
+  	function finalize ( string auth_token) public {
+
+  	
+  }
+  
+
+
 }
