d64bc45af0d9e4b8a0926733b73ca0252ad20293
asketak <simatomas@seznam.cz>
2019-10-28 14:22:54 +0100

    succesfully compiled, eventfactory stack too deep

--- smart_contract/contracts/ChainlinkExample.sol
+++ smart_contract/contracts/ChainlinkExample.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.24;
 
 import "chainlink/contracts/ChainlinkClient.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
--- smart_contract/contracts/EventFactory.sol
+++ smart_contract/contracts/EventFactory.sol
@@ -1,22 +1,12 @@
-import "./PredictEvent.sol";
+// import "./PredictEvent.sol";
 
-contract EventFactory {
-    address[] public allContracts;
+// contract EventFactory {
+//     mapping(address => uint) public allContracts;
 
-    function createContract (bytes32 name) {
-        address newContract = new PredictEvent(name);
-        newContracts.push(newContract);
-    } 
-
-    function getActiveContracts () {
-        address[] public ret;
-        for(uint x = 0; x<allContracts.length; x++){
-            if (allContracts[x].){
-
-                marketResolutionTimestamp
-                ret.push(newContract);
-            }
-        }
-    } 
-
-}
+//     function createContract ( string _name, uint _marketResolutionTimestamp, string _apiPath , string _httpPostOrGet ,
+//      string _getData , string _postData , string _jsonRegexString, uint[] _possibleOutcomes ) public {
+//         address newContract = new PredictEvent(_name,_marketResolutionTimestamp,_apiPath,_httpPostOrGet,
+//             _getData,_postData,_jsonRegexString,_possibleOutcomes);
+//         allContracts[newContract] = _marketResolutionTimestamp;
+//     } 
+// }
