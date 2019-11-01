896b8bfe7a926148091b5e86ca25077eb6b25994
N3xtGeN <x@n3xtgen.net>
2019-11-01 04:31:44 +0100

    Finish UI

--- frontend/src/CreateEventPage.jsx
+++ frontend/src/CreateEventPage.jsx
@@ -3,7 +3,7 @@ import React from "react"
 import TextField from '@material-ui/core/TextField';
 import AppContext from "./AppContext";
 
-import {Button, Card, CardContent} from "@material-ui/core";
+import {Button, Card, CardContent, FormControl, InputLabel, Select, MenuItem} from "@material-ui/core";
 
 import moment from "moment";
 
@@ -15,7 +15,9 @@ export default class CreateEventPage extends React.Component {
         super(props, context)
     }
 
-    state = {}
+    state = {
+        optionCount: 1
+    }
 
     componentDidMount() {
     }
@@ -23,23 +25,49 @@ export default class CreateEventPage extends React.Component {
     componentDidUpdate(prevProps, prevState, snapshot) {
     }
 
+    handleOptionCountChange = (e) => {
+        this.setState(state => ({optionCount: e.target.value}))
+    }
+
     createEventClick = (e) => {
         e.preventDefault()
         this.setState(state => ({running: true}))
         const request = {}
-        document.querySelectorAll("#create-event-form input").forEach(input => {
-            request[input.id] = (input.id === "timestamp" ? parseInt(input.value) : input.value)
+        document.querySelectorAll("#create-event-form .input-simple input, #create-event-form .input-simple textarea#description").forEach(input => {
+            request[input.id] = input.value
+        })
+        request.options = []
+        document.querySelectorAll("#create-event-form input[id^=optionM]").forEach(input => {
+            const optionId = input.id.substr(-1)
+            const minMax = input.id.startsWith("optionMin") ? "min": "max";
+
+            if (!request.options[optionId]) {request.options[optionId] = {}}
+
+            if (minMax === "min") {
+                request.options[optionId] = {...request.options[optionId], min: parseInt(input.value)}
+            } else {
+                request.options[optionId] = {...request.options[optionId], max: parseInt(input.value)}
+            }
         })
 
+        const requestOptionsMap = request.options.map(option => (["" + option.min + " - " + option.max, option.min, option.max, ""]))
+
+        debugger
         this.context.w3a.contracts.EventFactory.createContract.send(
             null,
-            request.name,
-            moment(request.endTimestamp).valueOf(),
-            request.apiPath,
-            request.postOrGet,
-            request.getData,
-            request.postData,
-            request.jsonRegex
+            [
+                request.name,
+                request.description,
+                request.apiPath,
+                request.postOrGet,
+                request.getData,
+                request.postData,
+                request.jsonRegex
+            ],
+            Math.floor(moment(request.endTimestamp).valueOf() / 1000),
+            requestOptionsMap,
+            false,
+            false
         )
             .then(rest => {
                 window.location.href = "/events";
@@ -57,11 +85,12 @@ export default class CreateEventPage extends React.Component {
             return <TextField
                 id={input.id}
                 label={input.label}
+                className="input-simple"
                 //defaultValue={input}
                 helperText={input.note}
                 margin="dense"
                 type="datetime-local"
-                defaultValue={moment().add(7, 'days').format("2017-05-24THH:mm")}
+                defaultValue={moment().add(1, 'hour').format("YYYY-MM-DDTHH:mm")}
                 InputLabelProps={{
                     shrink: true,
                 }}
@@ -70,7 +99,9 @@ export default class CreateEventPage extends React.Component {
         } else if (input.type === "multiline") {
             return <TextField
                 id={input.id}
+                type="text"
                 label={input.label}
+                className="input-simple"
                 //defaultValue={input}
                 helperText={input.note}
                 margin="dense"
@@ -82,7 +113,9 @@ export default class CreateEventPage extends React.Component {
         } else {
             return <TextField
                 id={input.id}
+                type="text"
                 label={input.label}
+                className="input-simple"
                 //defaultValue={input}
                 helperText={input.note}
                 margin="dense"
@@ -118,10 +151,83 @@ export default class CreateEventPage extends React.Component {
                                 <div className="row">
 
                                     {inputs.map(input =>
-                                        <div key={input.id} className="input-col col-lg-6">{this.renderInput(input)}</div>
+                                        <div key={input.id}
+                                             className="input-col col-lg-6">{this.renderInput(input)}</div>
                                     )}
 
-                                    <div key="submitButton" className="submit-col col-lg-6">
+                                    <div key="optionCount" className="input-col col-lg-12" style={{marginTop: "15px"}}>
+                                        <FormControl variant="outlined">
+                                            <InputLabel style={{width: "170px", fontSize: "25px"}}
+                                                        id="demo-simple-select-outlined-label">
+                                                Options count
+                                            </InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-outlined-label"
+                                                id="optionCount"
+                                                value={this.state.optionCount}
+                                                onChange={this.handleOptionCountChange}
+                                                labelWidth={160}
+                                                disabled={this.state.running}
+                                                margin="dense"
+                                                style={{
+                                                    width: "200px",
+                                                    marginTop: "10px",
+                                                    textAlign: "right",
+                                                    paddingRight: "10px",
+                                                    fontSize: "23px",
+                                                    fontWeight: "bold"
+                                                }}
+                                            >
+                                                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(option =>
+                                                    <MenuItem value={option}>
+                                                        {option}
+                                                    </MenuItem>
+                                                )}
+                                            </Select>
+                                        </FormControl>
+                                    </div>
+
+                                    {(() => {
+                                        const options = []
+                                        for (let i = 0; i < this.state.optionCount; i++) {
+                                            options.push(
+                                                <div>
+                                                    <h4>Range {i+1}:</h4>
+                                                    <div className="d-flex">
+                                                        <TextField
+                                                            type="number"
+                                                            style={{width: "90px", marginRight: "10px"}}
+                                                            id={"optionMin" + i}
+                                                            label="Min"
+                                                            helperText=""
+                                                            required
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="outlined"
+                                                            disabled={this.state.running}
+                                                        />
+                                                        <TextField
+                                                            type="number"
+                                                            style={{width: "90px", marginRight: "10px"}}
+                                                            id={"optionMax" + i}
+                                                            label="Max"
+                                                            helperText=""
+                                                            required
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="outlined"
+                                                            disabled={this.state.running}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                        return <div className="input-col col-lg-12"
+                                                    style={{width: "200px"}}>{options}</div>
+                                    })()}
+
+
+                                    <div key="submitButton" className="submit-col col-lg-12" style={{marginTop: "20px"}}>
                                         <Button size="large"
                                                 variant="contained"
                                                 color="primary"
@@ -130,7 +236,7 @@ export default class CreateEventPage extends React.Component {
                                             {
                                                 this.state.running ?
                                                     <div className="spinner-border text-warning" role="status">
-                                                        Loading...<span className="sr-only">Loading...</span>
+                                                        <span className="sr-only">Loading...</span>
                                                     </div>
                                                     :
                                                     <h4>
--- frontend/src/IntroContent.jsx
+++ frontend/src/IntroContent.jsx
@@ -3,6 +3,8 @@ import React from "react"
 import AppContext from "./AppContext.js";
 
 import deployedContracts from "./deployedContracts.js";
+import {Button} from "@material-ui/core";
+import {Link} from "react-router-dom";
 
 export default class IntroContent extends React.Component {
 
@@ -13,7 +15,7 @@ export default class IntroContent extends React.Component {
     state = {
         inited: false,
         addressCount: null,
-		allEvents: []
+        allEvents: []
     }
 
     componentDidMount() {
@@ -28,46 +30,29 @@ export default class IntroContent extends React.Component {
         if (force || (!this.state.inited && this.state.addressCount === null && this.context.w3a)) {
             this.setState({inited: true})
             this.EventFactory = this.context.w3a.contracts.EventFactory.getAllEvents.call()
-				.then(allEvents => {
-					this.setState(state => ({
-						allEvents: allEvents
-					}))
-				})
+                .then(allEvents => {
+                    this.setState(state => ({
+                        allEvents: allEvents
+                    }))
+                })
         }
     }
 
     render() {
         return (
             <section className="container">
-
-                <h2>Popular Markets:</h2>
-                <div className="row">
-                    {this.state.allEvents.map(event => (
-                            <div key={event.add} className="col-lg-4 mb-4">
-                                <div className="card h-100">
-                                    <h4 className="card-header">add: {event.add}</h4>
-                                    <div className="card-body">
-                                        <p className="card-text">timestamp: {event.timestamp}</p>
-                                    </div>
-                                    <div className="card-footer">
-                                        <a href="/markets/address" className="btn btn-primary">Not Impl</a>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    )}
-                </div>
-
-
-                <div className="row">
-                    <form id="test1">
-                        <div className="container">
-                            <div className="row">
-
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                <Link to="/events">
+                    <Button style={{
+                        display: "block",
+                        fontSize: "30px",
+                        fontWeight: "bold",
+                        padding: "30px",
+                        boxShadow: "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)",
+                        margin: "0 auto 30px"
+                    }}>
+                        Go To Events
+                    </Button>
+                </Link>
 
             </section>
         )
